a32a05426cba84ee3663c642585dd4001c959476
Lyndon <mcjohnsonlyndon@gmail.com>
2025-07-16 16:41:12 +0000

    ok

--- app/Filament/Resources/SpecialOfferResource/Api/Transformers/SpecialOfferTransformer.php
+++ app/Filament/Resources/SpecialOfferResource/Api/Transformers/SpecialOfferTransformer.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Filament\Resources\SpecialOfferResource\Api\Transformers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\SpecialOffer;
+use App\Filament\Resources\ItemResource\Api\Transformers\ItemTransformer;
 
 /**
  * @property SpecialOffer $resource
@@ -10,14 +12,21 @@ use App\Models\SpecialOffer;
 class SpecialOfferTransformer extends JsonResource
 {
 
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return $this->resource->toArray();
-    }
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function toArray($request)
+	{
+		$data = $this->resource->toArray();
+
+		// Include the related item object
+		if ($this->resource->relationLoaded('item') || $this->resource->item) {
+			$data['item'] = new ItemTransformer($this->resource->item);
+		}
+
+		return $data;
+	}
 }
