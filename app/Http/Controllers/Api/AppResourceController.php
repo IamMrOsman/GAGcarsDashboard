1737674f1df3486b3a3dded05b777ce147442cce
Lyndon <mcjohnsonlyndon@gmail.com>
2025-09-01 14:15:55 +0000

    added comments for api labeling

--- app/Http/Controllers/Api/AppResourceController.php
+++ app/Http/Controllers/Api/AppResourceController.php
@@ -44,31 +44,64 @@ class AppResourceController extends Controller
 		}
 	}
 
+	/**
+	 * Get Similar Items by Category
+	 * @param Category $category
+	 * @param Item $item
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function getSimilarItemsByCategory(Category $category, Item $item)
 	{
 		return response()->json($category->items()->where('id', '!=', $item->id)->get());
 	}
 
+	/**
+	 * Get Similar Items by Brand
+	 * @param Brand $brand
+	 * @param Item $item
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function getSimilarItemsByBrand(Brand $brand, Item $item)
 	{
 		return response()->json($brand->items()->where('id', '!=', $item->id)->get());
 	}
 
+	/**
+	 * Get Similar Items by Brand Model
+	 * @param BrandModel $brandModel
+	 * @param Item $item
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function getSimilarItemsByBrandModel(BrandModel $brandModel, Item $item)
 	{
 		return response()->json($brandModel->items()->where('id', '!=', $item->id)->get());
 	}
 
+	/**
+	 * Get Category Items
+	 * @param Category $category
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function getCategoryItems(Category $category)
 	{
 		return response()->json($category->items);
 	}
 
+	/**
+	 * Get Brand Items
+	 * @param Brand $brand
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function getBrandItems(Brand $brand)
 	{
 		return response()->json($brand->items);
 	}
 
+	/**
+	 * Get Brand Model Items
+	 * @param BrandModel $brandModel
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function getBrandModelItems(BrandModel $brandModel)
 	{
 		return response()->json($brandModel->items);
