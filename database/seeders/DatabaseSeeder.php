579c419bcbf178b18f9cda034cd0ca6e475b4793
LyndonMcjohnson <mcjohnsonlyndon@gmail.com>
2025-10-21 11:37:24 +0000

    brand seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,14 +8,15 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        $this->call([
-            CountrySeeder::class,
-            ItemFieldSeeder::class,
-        ]);
-    }
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void
+	{
+		$this->call([
+			CountrySeeder::class,
+			ItemFieldSeeder::class,
+			BrandSeeder::class,
+		]);
+	}
 }
