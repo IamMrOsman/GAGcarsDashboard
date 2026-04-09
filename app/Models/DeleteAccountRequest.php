ce09450242fbecddf422d5bc77fc8a3a0665558d
IamMrOsman <iammrosman@gmail.com>
2026-04-09 11:03:00 +0000

    fix

--- app/Models/DeleteAccountRequest.php
+++ app/Models/DeleteAccountRequest.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class DeleteAccountRequest extends Model
 {
+	use HasUlids;
+
 	protected $guarded = [];
+
+	public $incrementing = false;
+
+	protected $keyType = 'string';
 
 	protected $casts = [
 		'snapshot' => 'array',
